f01706e75cd87f238fed6c19cf0a5d0a18b9361e
ClaudiaJulian <claudia_e_julian@yahoo.com.ar>
2025-01-18 17:23:18 -0300

    Agrega Usuario Admin

--- src/main/resources/db/migration/V6__create-table-usuarios.sql
+++ src/main/resources/db/migration/V6__create-table-usuarios.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS usuarios(
     login VARCHAR(100) NOT NULL,
     password VARCHAR(300) NOT NULL,
 
-
     PRIMARY KEY(id)
-
 );
+
+INSERT INTO usuarios(id,login,password)
+VALUES(1,'maria.perez','$2y$10$8C8ZZjbEBF8SKsamZVgVWudn0WoReHs3cgdnrnuXGFIk62Ytku.sC');
